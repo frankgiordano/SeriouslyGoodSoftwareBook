ca91b85f1e38f817ae5e9b78b4c3b3739ee81c35
Frank Giordano <giofr01@yahoo.com>
2020-07-25 17:26:55 -0400

    chapter 3 - part1 - improvements

--- src/Container.java
+++ src/Container.java
@@ -2,31 +2,32 @@ import java.util.HashSet;
 import java.util.Set;
 
 /*
-Water containers , all identical and equipped with a virtually unlimited capacity. At any given time, a
+Water containers, all identical and equipped with a virtually unlimited capacity. At any given time, a
 container holds a certain amount of liquid, and any two containers can be permanently connected by a pipe.
 Instead of sending messages, you can pour water in or remove it from a container. Whenever two or more containers
 are connected, they become communicating vessels and from that time on they split equally the liquid contained in them.
  */
 public class Container {
 
-    private Set<Container> group;
-    private double amount;
+    private Group group = new Group(this);
 
-    public Container() {
-        group = new HashSet<Container>();
-        group.add(this);
-        amount = 0;
+    private static class Group {
+        double amountPerContainer;
+        Set<Container> members; // #1 The set of all connected containers
+
+        public Group(Container c) {
+            members = new HashSet<Container>();
+            members.add(c);
+        }
     }
 
     public double getAmount() {
-        return amount;
+        return group.amountPerContainer;
     }
 
     public void addWater(double amount) {
-        double amountPerContainer = amount / group.size();
-        for (Container c: group) {
-            c.amount += amountPerContainer;
-        }
+        double amountPerContainer = amount / group.members.size();
+        group.amountPerContainer = amountPerContainer;
     }
 
     public void connectTo(Container other) {
@@ -34,19 +35,17 @@ public class Container {
         // If they are already connected, do nothing
         if (group == other.group) return;
 
-        int size1 = group.size();
-        int size2 = other.group.size();
-        double tot1 = amount * size1;
-        double tot2 = other.amount * size2;
+        int size1 = group.members.size();
+        int size2 = other.group.members.size();
+        double tot1 = group.amountPerContainer * size1;
+        double tot2 = other.group.amountPerContainer * size2;
         double newAmount = (tot1 + tot2) / (size1 + size2);
 
         // Merge the two groups
-        group.addAll(other.group);
+        group.members.addAll(other.group.members);
+        group.amountPerContainer = newAmount;
 
-        for (Container c: other.group)
-            c.group = group;
-        for (Container c: group)
-            c.amount = newAmount;
-
+        for (Container x: other.group.members)
+            x.group = group;
     }
 }
